641ef0e1614c23dd9c1436635f5c7c3d3a0fc5e7
Edwin Kofler <edwin@kofler.dev>
2022-08-11 16:00:20 -0700

    meta: Update dependencies

--- test/first.test.js
+++ test/first.test.js
@@ -1,4 +1,5 @@
 import path from 'path'
+import url from 'url'
 import fs from 'fs'
 import test from 'ava'
 import { transformAsync } from '@babel/core'
@@ -6,19 +7,24 @@ import plugin from '../index.js'
 
 test('testing for match', async (t) => {
 	const input = await fs.promises.readFile(
-		path.join(__dirname, 'fixtures/one.in.js'),
-		{ encoding: 'utf8' }
+		path.join(
+			path.dirname(url.fileURLToPath(import.meta.url)),
+			'fixtures/one.in.js',
+		),
+		{ encoding: 'utf8' },
 	)
 	const output = await fs.promises.readFile(
-		path.join(__dirname, 'fixtures/one.out.js'),
-		{ encoding: 'utf8' }
+		path.join(
+			path.dirname(url.fileURLToPath(import.meta.url)),
+			'fixtures/one.out.js',
+		),
+		{ encoding: 'utf8' },
 	)
 	try {
 		const { code } = await transformAsync(input, {
 			plugins: [plugin],
 			ast: true,
 		})
-		fs.promises.writeFile('temp', code)
 		t.is(output, code)
 	} catch (err) {
 		console.error(err)
